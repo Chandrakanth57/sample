155ce4341390645f0608234ff56db3a3ec1becb5
chandrakanth <chandrakanthchandru0799@gmail.com>
2024-08-09 22:17:59 +0530

    forever

--- v.py
+++ v.py
@@ -1,9 +1,8 @@
 def hateu():
     print("from chandrakanth,..")
-    print("\t\t\tSorry vaishu")
-    print("\t\t\tlove you vaishu")
-    print("\t\t\tpooja, teena all are my sisters vaishu")
-    print("\t\tyou r my world, life everything")
+    print("\t\t\tSorry vaishu You are the best gift I git from this universe ")
+    print("\t\t\tI am blessed you have you in my life")
+    print("Don't leave me and go please 🙏")    
 
 while True:
     hateu() 
