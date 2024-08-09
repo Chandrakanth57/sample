7a0c20a283944cb98eea7c679e9661fc8b027ad0
vaishnavi <vaishnavibk4@gmail.com>
2024-08-09 22:08:30 +0530

    h

---
+++ v.py
@@ -0,0 +1,9 @@
+def hateu():
+    print("from chandrakanth,..")
+    print("\t\t\tSorry vaishu")
+    print("\t\t\tlove you vaishu")
+    print("\t\t\tpooja, teena all are my sisters vaishu")
+    print("\t\tyou r my world, life everything")
+
+while True:
+    hateu() 
